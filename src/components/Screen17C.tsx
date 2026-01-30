6bcda259cb1401141084e6b0fbfb56b9f7347e91
annaintend <anna@intend.com>
2026-01-30 15:17:10 +0200

    Update Screen17C.tsx

--- src/components/Screen17C.tsx
+++ src/components/Screen17C.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { motion } from 'framer-motion';
-import imgSugarnologooo1 from "figma:asset/90d0b2808b9f2d4ad23a49432895256cef99dbdf.png";
+import imgSugarnologooo1 from "@/assets/sugarno-black-logo.png";
 
 interface Screen17CProps {
   onNext: (data?: Record<string, any>) => void;
